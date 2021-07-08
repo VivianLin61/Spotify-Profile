0fda647173a1644628dd4cd676d32b06ad1353ac
Vivian Lin <linvivian61@gmail.com>
2021-07-08 08:48:15 -0400

    song player

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import useAuth from '../hooks/useAuth.js'
+import { Container, Form } from 'react-bootstrap'
+import SpotifyWebApi from 'spotify-web-api-node'
+import TrackSearchResult from '../components/TrackSearchResult.js'
+import Player from '../components/Player.js'
+import axios from 'axios'
 
+const spotifyApi = new SpotifyWebApi({
+  clientId: '8515d3f514614d3da7c8e1b533e664c2',
+})
 function Dashboard({ code }) {
   const accessToken = useAuth(code)
-  return <div>{code}</div>
+  const [search, setSearch] = useState('')
+  const [searchResults, setSearchResults] = useState([])
+  const [playingTrack, setPlayingTrack] = useState()
+  const [lyrics, setLyrics] = useState('')
+  function chooseTrack(track) {
+    setPlayingTrack(track)
+    setSearch('')
+    setLyrics('')
+  }
+
+  useEffect(() => {
+    if (!accessToken) return
+    spotifyApi.setAccessToken(accessToken)
+  }, [accessToken])
+
+  useEffect(() => {
+    if (!search) return setSearchResults([])
+    if (!accessToken) return
+    let cancel = false
+
+    spotifyApi.searchTracks(search).then((res) => {
+      if (cancel) return
+      setSearchResults(
+        res.body.tracks.items.map((track) => {
+          const smallestAlbumImage = track.album.images.reduce(
+            (smallest, image) => {
+              if (image.height < smallest.height) return image
+              return smallest
+            },
+            track.album.images[0]
+          )
+
+          return {
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: smallestAlbumImage.url,
+          }
+        })
+      )
+    })
+    //If a new request it made while seraching cancel the request.
+    return () => (cancel = true)
+  }, [search, accessToken])
+  return (
+    <Container className='d-flex flex-column py-2' style={{ height: '100vh' }}>
+      <Form.Control
+        type='search'
+        placeholder='Search Songs/Artists'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className='flex-grow-1 my-2' style={{ overflowY: 'auto' }}>
+        {searchResults.map((track) => (
+          <TrackSearchResult track={track} key={track.uri} />
+        ))}
+        {searchResults.length === 0 && (
+          <div className='text-center' style={{ whiteSpace: 'pre' }}></div>
+        )}
+      </div>
+      <div>
+        <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
+      </div>
+    </Container>
+  )
 }
 
 export default Dashboard
